4c6e1f5aa1940e4262a10b9d73d5c72412b075fd
Nenad Opancarev <nopancarev@gmail.com>
2021-11-16 17:39:32 +0100

    add  JavaScript

--- main.js
+++ main.js
@@ -4,9 +4,18 @@ function moveLeft() {
   left = left - 10;
   document.getElementById('circle').style.marginLeft = left + 'px';
 }
-
-
 function moveRight() {
   left = left + 10;
   document.getElementById('circle').style.marginLeft = left + 'px';
+}
+
+var up = 0;
+
+function moveUp() {
+  up = up - 10;
+  document.getElementById('circle').style.marginTop = up + 'px';
+}
+function moveDown() {
+  up = up + 10;
+  document.getElementById('circle').style.marginTop = up + 'px';
 }
