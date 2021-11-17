61c9db3d32e85e38082c360c8fa163162be54936
Nenad Opancarev <nopancarev@gmail.com>
2021-11-17 12:01:59 +0100

    event listeners

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 var left = 0;
+var up = 0;
+
+document.getElementById("btnLeft").addEventListener("click", moveLeft);
+document.getElementById("btnRight").addEventListener("click", moveRight);
+document.getElementById("btnUp").addEventListener("click", moveUp);
+document.getElementById("btnDown").addEventListener("click", moveDown);
+document.addEventListener("keydown", function (event) {
+  if (event.key === 'ArrowLeft') {
+    moveLeft()
+  }
+  if (event.key === 'ArrowRight') {
+    moveRight()
+  }
+  if (event.key === 'ArrowUp') {
+    moveUp()
+  }
+  if (event.key === 'ArrowDown') {
+    moveDown()
+  }
+}, true)
 
 function moveLeft() {
   left = left - 10;
   document.getElementById('circle').style.marginLeft = left + 'px';
 }
+
 function moveRight() {
   left = left + 10;
   document.getElementById('circle').style.marginLeft = left + 'px';
 }
 
-var up = 0;
-
 function moveUp() {
   up = up - 10;
   document.getElementById('circle').style.marginTop = up + 'px';
 }
+
 function moveDown() {
   up = up + 10;
   document.getElementById('circle').style.marginTop = up + 'px';
